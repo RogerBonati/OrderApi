c3f6f2450d68375303af53f9b3c0ef476550a8c4
Roger Bonati <roger.bonati@web.de>
2024-05-20 23:45:13 +0200

    Basic webserver added

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 
-	fmt.Println("Hello Sepp!")
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Failed to listen to server: ", err)
+	}
+
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	// cast inputstring to bytearray
+	_, err := w.Write([]byte("Servus Sepp!"))
+	if err != nil {
+		fmt.Println("Server could not write message: ", err)
+	}
 }
