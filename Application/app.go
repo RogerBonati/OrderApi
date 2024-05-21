cffa3b7087fe6558b174eec3c6792a641e11a5fe
Roger Bonati <roger.bonati@web.de>
2024-05-22 00:10:47 +0200

    Bugfixes

--- Application/app.go
+++ Application/app.go
@@ -1,41 +1,33 @@
 package application
 
-import {
-	
-	"fmt"
-	"net/http"
-	"github.com/go-chi/chi/v5"
+import (
 	"context"
-}
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
 
 type App struct {
 	router http.Handler
 }
 
-
-
 // build the constructor for the application
 
 func New() *App {
 	app := &App{
-		router: loadRoutes()
-
-		
+		router: loadRoutes(),
 	}
 	return app
 }
 
 func (a *App) Start(ctx context.Context) error {
 	server := &http.Server{
-		Addr:			":3000",
-		Handler:	a.router,
+		Addr:    ":3000",
+		Handler: a.router,
 	}
-	err := server.ListenAndServer()
+	err := server.ListenAndServe()
 	if err != nil {
 		return fmt.Errorf("Failed to start server: %w", err)
 	}
 	return nil
 }
-
-
-
